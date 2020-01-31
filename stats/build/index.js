b4149422592d79a6f695532796d8129fb0065843
Alin <adumitru@bitdefender.com>
2020-01-31 17:31:47 +0200

    more stats app

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,18 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var CsvFileReader_1 = require("./CsvFileReader");
-var MatchResult_1 = require("./MatchResult");
-var reader = new CsvFileReader_1.CsvFileReader('football.csv');
-reader.read();
-console.log(reader.data[0]);
-var manUnitedWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
+var MatchReader_1 = require("./MatchReader");
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+console.log(matchReader.matches);
+// const reader = new MatchReader('football.csv');
+// reader.read();
+// console.log(reader.data[0]);
 console.log("Man United won " + manUnitedWins + " games");
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,16 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+console.log(matchReader.matches);
 
-console.log(reader.data[0]);
+// const reader = new MatchReader('football.csv');
+// reader.read();
+// console.log(reader.data[0]);
 
-let manUnitedWins = 0;
 
-for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
 
 console.log(`Man United won ${manUnitedWins} games`);

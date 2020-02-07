28e6a5d89172e6da3b4a63ab91aa4ce50714f334
Alin <adumitru@bitdefender.com>
2020-02-07 17:46:13 +0200

    more web progress

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,26 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
-user.fetch();
+const user = new User({ id: 1, name: 'new name', age: 15 });
 
-setTimeout(() => console.log(user), 4000);
+user.on('save', () => {
+    console.log(user);
+});
 
-// user.set({ name: 'alin' });
-
-// user.on('change', () => {
-//     console.log('#1');
-// });
-// user.on('change', () => {
-//     console.log('#2');
-// });
-// user.on('save', () => {
-//     console.log('#3');
-// });
-
-// user.trigger('click');
-
-// import axios from 'axios';
-// axios.post('http://localhost:3000/users', {
-//     name: 'alin',
-//     age: 20
-// });
+user.save();
--- web/src/models/Eventing.ts
+++ web/src/models/Eventing.ts
@@ -2,19 +2,19 @@ type Callback = () => void;
 
 export class Eventing {
     events: { [key: string]: Callback[] } = {};
-    
-    on(eventName: string, callback: Callback): void {
+
+    on = (eventName: string, callback: Callback): void => {
         const handlers = this.events[eventName] || [];
 
         handlers.push(callback);
         this.events[eventName] = handlers;
-    }
+    };
 
-    trigger(eventName: string): void {
+    trigger = (eventName: string): void => {
         const handlers = this.events[eventName] || [];
 
         handlers.forEach(handler => {
             handler();
         });
-    }
+    };
 }
